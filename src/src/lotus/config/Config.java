c7106c13a92f088d52851439b664abd2059a1b6c
zh-or <982465979@qq.com>
2017-08-25 15:22:50 +0800

    test

--- src/src/lotus/config/Config.java
+++ src/src/lotus/config/Config.java
@@ -5,6 +5,7 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileWriter;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.Map;
@@ -15,11 +16,38 @@ import java.util.Map;
  */
 public class Config {
 	
-	private HashMap<String, HashMap<String, String>> group = null;
+	private HashMap<String, HashMap<String, ArrStringValue>> group = null;
 	private File file;
+	
+	public class ArrStringValue{
+	    private ArrayList<String> val;
+	    
+	    public ArrStringValue(){
+	        val = new ArrayList<String>();
+	    }
+
+	    public ArrStringValue put(String v){
+	        val.add(v);
+	        return this;
+	    }
+	    
+	    public ArrayList<String> getVal(){
+	        return val;
+	    }
+	    
+        @Override
+        public String toString() {
+            if(val.size() > 0){
+                return val.get(0);
+            }
+            return null;
+        }
+	    
+	}
+	
 
 	public Config(File f){
-		group = new HashMap<String, HashMap<String,String>>();
+		group = new HashMap<String, HashMap<String, ArrStringValue>>();
 		this.file = f;
 	}
 	
@@ -34,12 +62,12 @@ public class Config {
 		BufferedReader bis = null;
 		try {
 			bis = new BufferedReader(new InputStreamReader(new FileInputStream(file), charset));
-			HashMap<String, String> child = null;
+			HashMap<String, ArrStringValue> child = null;
 			String line = null;
 			while (null != (line = bis.readLine())){
 			//	System.out.println("line:" + line);
 				if('[' == line.charAt(0) && ']' == line.charAt(line.length() - 1)){
-					child  = new HashMap<String, String>();
+					child  = new HashMap<String, ArrStringValue>();
 					child.clear();
 					String gname = line.substring(1, line.length() - 1);
 					group.put(gname, child);
@@ -51,7 +79,13 @@ public class Config {
 					cvalue = line.substring(p + 1, line.length());
 					cname = cname.trim();
 					cvalue = cvalue.trim();
-					child.put(cname, cvalue);
+					ArrStringValue v = child.get(cname);
+					if(v != null){//如果这个那么已经存在
+					    v.put(cvalue);
+					}else{
+					    child.put(cname, new ArrStringValue().put(cvalue));
+					}
+					
 				}
 				
 			}
@@ -75,10 +109,18 @@ public class Config {
 	 * @return
 	 */
 	public String getStringValue(String groupkey, String childkey, String defaultvalue){
-		HashMap<String, String>  child = group.get(groupkey);
-		if(child == null) return defaultvalue;
-		String v = child.get(childkey);
-		return (v == null || "".equals(v)) ? defaultvalue : v;
+	    ArrStringValue val = getArrStringValue(groupkey, childkey, defaultvalue);
+		return val.toString();
+	}
+	
+	public ArrStringValue getArrStringValue(String groupkey, String childkey, String defaultvalue){
+	    HashMap<String, ArrStringValue>  child = group.get(groupkey);
+        if(child == null) return new ArrStringValue().put(defaultvalue);
+        ArrStringValue val = child.get(childkey);
+        if(val == null){
+            return new ArrStringValue().put(defaultvalue);
+        }
+        return val;
 	}
 	
 	public int getIntValue(String groupkey, String childkey, int defaultvalue){
@@ -98,47 +140,35 @@ public class Config {
 	
 	
 	/**
-	 * 设置值 如果有则覆盖 没有则创建
+	 * 设置值 没有则创建
 	 * @param groupkey
 	 * @param childkey
 	 * @param value
 	 * @return
 	 */
-	public String putValue(String groupkey, String childkey, String value){
-		HashMap<String, String>  child = group.get(groupkey);
+	public void putValue(String groupkey, String childkey, String value){
+		HashMap<String, ArrStringValue>  child = group.get(groupkey);
 		if(child == null){
-			child = new HashMap<String, String>();
-			child.put(childkey, value);
+			child = new HashMap<String, ArrStringValue>();
+			child.put(childkey, new ArrStringValue().put(value));
 			group.put(groupkey, child);
-			return null;
+			return ;
 		}
-		return child.put(childkey, value);
+		ArrStringValue val = child.get(childkey);
+		if(val == null){
+		    child.put(childkey, new ArrStringValue().put(value));
+		}else{
+		    val.put(value);
+		}
 	}
 	
 	public void save(){
 		
 		FileWriter fout = null;
 		try {
-			StringBuilder sb = new StringBuilder();
-			fout = new FileWriter(file);
-			Iterator<?> iter = group.entrySet().iterator();
-			while (iter.hasNext()) {
-				@SuppressWarnings("rawtypes")
-				Map.Entry entry = (Map.Entry) iter.next();
-				String key = (String) entry.getKey();
-				@SuppressWarnings("unchecked")
-				HashMap<String, String> val = (HashMap<String, String>) entry.getValue();
-				sb.append("[" + key + "]\n");
-				Iterator<?> vs = val.entrySet().iterator();
-				while(vs.hasNext()){
-					@SuppressWarnings("rawtypes")
-					Map.Entry c_entry = (Map.Entry) vs.next();
-					String k = (String) c_entry.getKey();
-					String v = (String) c_entry.getValue();
-					sb.append(k + "=" + v + "\n");
-				}
-			}
-			fout.write(sb.toString());
+	        fout = new FileWriter(file);
+			
+			fout.write(build().toString());
 			fout.flush();
 		} catch (Exception e) {
 			e.printStackTrace();
@@ -150,10 +180,38 @@ public class Config {
 			}
 		}
 	}
+	
+	public StringBuilder build(){
+	    StringBuilder sb = new StringBuilder();
+        Iterator<?> iter = group.entrySet().iterator();
+        while (iter.hasNext()) {
+            @SuppressWarnings("rawtypes")
+            Map.Entry entry = (Map.Entry) iter.next();
+            String key = (String) entry.getKey();
+            @SuppressWarnings("unchecked")
+            HashMap<String, ArrStringValue> val = (HashMap<String, ArrStringValue>) entry.getValue();
+            sb.append("[" + key + "]\n");
+            Iterator<?> vs = val.entrySet().iterator();
+            while(vs.hasNext()){
+                @SuppressWarnings("rawtypes")
+                Map.Entry c_entry = (Map.Entry) vs.next();
+                String k = (String) c_entry.getKey();
+                ArrStringValue v = (ArrStringValue) c_entry.getValue();
+                for(String tv : v.getVal()){
+                    sb.append(k);
+                    sb.append("=");
+                    sb.append(tv);
+                    sb.append("\n");
+                }
+                
+            }
+        }
+        return sb;
+	}
 
 	@Override
 	public String toString() {
-		return "Config [group=" + group + "]";
+		return "\nConfig [\n" + build() + "\n]";
 	}
 	
 }
