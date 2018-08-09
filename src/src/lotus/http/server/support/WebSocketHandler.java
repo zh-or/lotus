abc349cf3607f220d8010cc2df9a90e296093ed6
O_R <982465979@qq.com>
2018-08-09 17:54:04 +0800

    123

--- src/src/lotus/http/server/support/WebSocketHandler.java
+++ src/src/lotus/http/server/support/WebSocketHandler.java
@@ -1,5 +1,9 @@
 package lotus.http.server.support;
 
+import java.net.URLDecoder;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 import lotus.http.server.WsRequest;
 import lotus.http.server.WsResponse;
 import lotus.nio.Session;
@@ -20,5 +24,16 @@ public abstract class WebSocketHandler {
     
     public void WebSocketPing(Session session){
         session.write(WsResponse.pong());
+    }
+    
+    
+    public String getParameter(String name, String path) {
+        try {
+            Matcher m = Pattern.compile("[&?]" + name + "=([^&]*)").matcher("&" + path);
+            if(m.find()){
+                return URLDecoder.decode(m.group(1), "utf-8");
+            }
+        } catch (Exception e) { }
+        return null;
     }
 }
