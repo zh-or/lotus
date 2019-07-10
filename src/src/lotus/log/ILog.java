f6a9d5d096c513b612545d53c16eece6bdfbb127
or <982465979@qq.com>
2019-07-10 21:13:07 +0800

    logger

--- src/src/lotus/log/ILog.java
+++ src/src/lotus/log/ILog.java
@@ -7,14 +7,19 @@ public interface ILog {
     public static final int L_W        = 1;
     public static final int L_I        = 2;
     public static final int L_D        = 3;
+    public static final int L_T        = 4;
 
-    static final String lvl[]             =   {"[ERROR]", "[WARN] ", "[INFO] ", "[DEBUG]"};
+    static final String lvl[]             =   {"[ERROR]", "[WARN] ", "[INFO] ", "[DEBUG]", "[TRACE]"};
     
     public void log(String str);
     
     public void log(int l, String str, Object ...args);
     
     public void log(int l, String str);
+    
+    public void trace(String str);
+
+    public void trace(String str, Object ...args);
     
     public void info(String str);
     
