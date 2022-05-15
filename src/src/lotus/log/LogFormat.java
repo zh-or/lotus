a33186dbe593aacbf51521bf923e82e4441a38fd
yangfan <982465979@qq.com>
2022-05-15 21:16:49 +0800

    log ignore change

---
+++ src/src/lotus/log/LogFormat.java
@@ -0,0 +1,87 @@
+package lotus.log;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.TimeZone;
+
+import lotus.log.Log.LogFilter;
+
+public class LogFormat {
+
+    private static LogFormat format               =   null;
+    private static Object    _lock                =   new Object();
+
+    private SimpleDateFormat sdf                  =   new SimpleDateFormat("MM-dd HH:mm:ss");
+    private LogWriter        writer               =   null;
+    private LogFilter        logfilter            =   null;
+    private boolean          debug_enable         =   true;
+
+    private String           PROJECT_NAME         =   "";
+
+    public static LogFormat getLogFormat() {
+        if(format == null) {
+            synchronized (_lock) {
+                if(format == null) {
+                    format = new LogFormat();
+                }
+            }
+        }
+        return format;
+    }
+
+    /**
+     * 这只日志文件保存目录, 按天保存
+     * @param dir
+     */
+    public synchronized void setLogFileDir(String dir) {
+        if(writer == null) {
+            writer = new LogWriter();
+        }
+        writer.setDir(dir);
+    }
+
+
+    /**
+     * 设置时区
+     * @param id GMT+8
+     */
+    public void setTimeZoneID(String id) {
+        sdf.setTimeZone(TimeZone.getTimeZone(id));
+    }
+
+    public void setProjectName(String name){
+        PROJECT_NAME = "[" + name + "]";
+    }
+
+    public void setLogFilter(LogFilter logfilter){
+        this.logfilter = logfilter;
+    }
+
+    public boolean getDebugEnable() {
+        return debug_enable;
+    }
+
+    public void setDebugEnable(boolean isEnable){
+        debug_enable = isEnable;
+    }
+
+    private static final String NULL_STR = "";
+
+    public void print(int l, String clazzName, String str) {
+        String cname = NULL_STR;
+        if(clazzName != null){
+            cname = "[" + clazzName + "]";
+        }
+
+        String msg_ = sdf.format(new Date());
+        msg_ = String.format("%s %s%s%s %s", msg_, PROJECT_NAME, cname, Log.lvl[l], str);
+        if(logfilter == null || logfilter.log(l, msg_)) {
+            if(writer != null) {//写到文件
+                writer.write(msg_);
+            }
+            System.out.println(msg_);
+            System.out.flush();
+        }
+    }
+
+}
