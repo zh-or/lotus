ac0f926f1389565abfc7b47e3a22f7e544c95b8e
yangfan <982465979@qq.com>
2023-01-30 16:28:56 +0800

    fix bug

--- src/src/lotus/nio/tcp/NioTcpServer.java
+++ src/src/lotus/nio/tcp/NioTcpServer.java
@@ -16,97 +16,106 @@ import java.util.concurrent.locks.ReentrantLock;
 import lotus.nio.IoHandler;
 import lotus.nio.NioContext;
 import lotus.utils.Utils;
+import lotus.nio.IoEventRunnable.IoEventType;
 
 public class NioTcpServer extends NioContext{
-	private ServerSocketChannel ssc			=	null;
-	private NioTcpIoProcess     ioprocess[] =   null;
+    private ServerSocketChannel ssc			=	null;
+    private NioTcpIoProcess     ioprocess[] =   null;
     private int                 iipBound    =   0;
     private final ReentrantLock rliplock    =   new ReentrantLock();
-	private AcceptThread        acceptRunner=   null;
+    private AcceptThread        acceptRunner=   null;
     private Thread              acceptThread=   null;
-	private long                idcount     =   0l;
-	private boolean             tcpNoDelay  =   false;
-	
+    private long                idcount     =   0l;
+    private boolean             tcpNoDelay  =   false;
+
     public NioTcpServer() {
-		super();
-	}
+        super();
+    }
 
     public void start(InetSocketAddress addr) throws IOException {
-        
+
         if(this.handler == null) {
             this.handler = new IoHandler() { };
         }
-        
+
         this.bufferlist = new LinkedBlockingQueue<ByteBuffer>(buffer_list_length);
-        
+
         ssc = ServerSocketChannel.open();
         //不用设置此值, 看起来操作系统会自动优化
         //ssc.socket().setReceiveBufferSize(buff_cache_size);
         ssc.configureBlocking(false);
         ioprocess = new NioTcpIoProcess[selector_thread_total];
-        
+
         for(int i = 0; i < selector_thread_total; i++) {
             ioprocess[i] = new NioTcpIoProcess(this);
             Thread tmpThread = new Thread(ioprocess[i], "lotus nio tcp server selector thread - " + i);
             ioprocess[i].setThread(tmpThread);
             tmpThread.start();
         }
-        
+
         if(event_pool_thread_size > 0) {
             this.executor = Executors.newFixedThreadPool(event_pool_thread_size, new ThreadFactory() {
-                
+
                 @Override
                 public Thread newThread(Runnable r) {
                     return new Thread(r, "lotus nio tcp server default fixed thread pool");
                 }
             });
         }
-        
+
         /*一个线程 accept */
         acceptRunner = new AcceptThread();
         acceptThread = new Thread(acceptRunner, "lotus nio tcp accept thread");
         acceptThread.start();
-        
+
         ssc.bind(addr);
     }
-    
+
     public boolean isTcpNoDelay() {
         return tcpNoDelay;
     }
-    
+
     public void setTcpNoDelay(boolean isNoDelay) {
         tcpNoDelay = isNoDelay;
     }
-	
-	public void close() {
-		try {
+
+    public void close() {
+        try {
             acceptRunner.close();
             acceptThread.join(20000);
-            if(executor != null) executor = null;
-		    for(int i = 0; i < selector_thread_total; i++){
-		        try {
-		            ioprocess[i].close();
+            if(executor != null) {
+                try {
+                    ((ExecutorService) executor).shutdown();
+                    ((ExecutorService) executor).awaitTermination(10, TimeUnit.SECONDS);
+                } catch(Exception e2) {
+                    e.printStackTrace();
+                }
+                executor = null;
+            }
+            for(int i = 0; i < selector_thread_total; i++){
+                try {
+                    ioprocess[i].close();
                     ioprocess[i].joinThread(20000);
                 } catch (Exception e) { }
-		    }
+            }
             bufferlist.clear();
-		    if(ssc != null) ssc.close();
-		    ssc = null;
+            if(ssc != null) ssc.close();
+            ssc = null;
         } catch (Exception e) {}
-	}
-	
+    }
+
     private class AcceptThread implements Runnable{
         Selector mslAccept = null;
         volatile boolean run = true;
         public AcceptThread() throws IOException {
             mslAccept = Selector.open();
         }
-        
+
         public void close(){
             run = false;
             mslAccept.wakeup();
         }
-        
+
         @Override
         public void run() {
             try {
@@ -114,14 +123,14 @@ public class NioTcpServer extends NioContext{
                 while(mslAccept != null && run){
                     try {
                         if(mslAccept.select(SELECT_TIMEOUT) == 0){
-                            
+
                             if(!run) break;
-                            
+
                             Utils.SLEEP(1);
                             continue;
                         }
                         Iterator<SelectionKey> keys = mslAccept.selectedKeys().iterator();
-                        
+
                         while(keys.hasNext()){
                             SelectionKey key = keys.next();
                             keys.remove();
@@ -162,8 +171,8 @@ public class NioTcpServer extends NioContext{
                         }
                     } catch (IOException e) {
                         e.printStackTrace();
-                    } 
-                    
+                    }
+
                 }
                 mslAccept.close();
             } catch (IOException e) {
@@ -172,5 +181,5 @@ public class NioTcpServer extends NioContext{
         }
     }
 
-	
+
 }
