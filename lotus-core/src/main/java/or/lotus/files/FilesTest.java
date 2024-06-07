e085a5c245b1da797beea82a9023c666d87d6c2e
yangfan <982465979@qq.com>
2024-06-07 17:47:21 +0800

    文件管理

--- lotus-core/src/main/java/or/lotus/files/FilesTest.java
+++ lotus-core/src/main/java/or/lotus/files/FilesTest.java
@@ -1,18 +1,18 @@
 package or.lotus.files;
 
 
+import or.lotus.support.Utils;
+
 import java.io.IOException;
 import java.net.InetSocketAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
 
 public class FilesTest {
-    public static void main(String[] args) throws IOException {
-        ServerSocket server = new ServerSocket();
-        server.bind(new InetSocketAddress(9527));
-        Socket socket = server.accept();
+    public static void main(String[] args) throws Exception {
+        FileManager fm = new FileManager("./test", Utils.formatSize("10GB"), "utf-8");
+        fm.put("haha", "haha de content");
 
-        socket.getOutputStream();
-
+        System.out.println("返回:" + fm.getString("haha"));
     }
 }
