e4a96811a5ed0f59abbb921d409f1c00eb0d0f02
yangfan <982465979@qq.com>
2024-07-02 18:02:33 +0800

    orm

---
+++ lotus-orm/src/main/java/lotus/or/orm/db/DatabaseExecutor.java
@@ -0,0 +1,190 @@
+package lotus.or.orm.db;
+
+import lotus.or.orm.pool.LotusDataSource;
+import or.lotus.common.Utils;
+import or.lotus.json.BeanBase;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.math.BigDecimal;
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+public class DatabaseExecutor<T> {
+    static final Logger log = LoggerFactory.getLogger(DatabaseExecutor.class);
+    String sqlMethod;//select insert update delete
+    Class<T> clazz;
+    LotusSqlBuilder builder;
+    Database db;
+    ArrayList<Object> params = new ArrayList<>(20);
+
+    public DatabaseExecutor(Database db, String m, Class<T> clazz) {
+        this.db = db;
+        this.sqlMethod = m;
+        this.clazz = clazz;
+        builder = new LotusSqlBuilder(JdbcUtils.convertPropertyNameToUnderscoreName(clazz.getSimpleName()));
+    }
+
+    public LotusSqlBuilder getSqlBuilder() {
+        return builder;
+    }
+
+    public DatabaseExecutor<T> fieldsFromObj() {
+        Field[] fields = clazz.getDeclaredFields();
+        List<String> fieldNames = new ArrayList<>(fields.length);
+        for(Field f : fields) {
+            fieldNames.add(JdbcUtils.convertPropertyNameToUnderscoreName(f.getName()));
+        }
+        fieldList(fieldNames);
+        return this;
+    }
+
+    public DatabaseExecutor<T> fields(String ...fs) {
+        List<String> fieldNames = new ArrayList<>(fs.length);
+        for(String f : fs) {
+            fieldNames.add(f);
+        }
+        fieldList(fieldNames);
+        return this;
+    }
+
+    public DatabaseExecutor<T> fieldList(List<String> fs) {
+        builder.setFields(fs);
+        return this;
+    }
+
+    public DatabaseExecutor<T> params(Object ...ps) {
+        for(Object p : ps) {
+            params.add(p);
+        }
+        return this;
+    }
+    public int exec() {
+        return 0;
+    }
+
+    public int findCount() {
+        return 0;
+    }
+
+    public T findOne() {
+        List<T> res = runSelect(true);
+        if(res != null && res.size() > 0) {
+            return res.get(0);
+        }
+        return null;
+    }
+
+    public List<T> findList() {
+        return runSelect(true);
+    }
+
+
+    public List<T> runSelect(boolean one) {
+        ResultSet rs = null;
+        try (Connection conn = db.getConnection();
+             PreparedStatement ps = conn.prepareStatement(builder.buildSelect());
+        ) {
+
+            setParamsToStatement(ps);
+            rs = ps.executeQuery();
+            ResultSetMetaData metaData = rs.getMetaData();
+            int columnCount = metaData.getColumnCount();
+
+            List<T> resObj = new ArrayList<T>();
+            while(rs.next()) {
+                T obj = clazz.newInstance();
+                Class<?> newClazz = obj.getClass();
+                for(int i = 1; i <= columnCount; i++) {
+                    String name = metaData.getColumnName(i);
+                    try {
+                        String fieldName = JdbcUtils.convertUnderscoreNameToPropertyName(name, false);
+                        String setMethodName = "set" + JdbcUtils.convertUnderscoreNameToPropertyName(name, true);
+
+                        Field field = newClazz.getField(fieldName);
+                        Method m = newClazz.getMethod(setMethodName, field.getType());
+
+                        Object val = JdbcUtils.getResultSetValue(rs, i, field.getType());
+                        m.invoke(obj, val);
+                        resObj.add((T) resObj);
+                    } catch (NoSuchFieldException e) {
+                        log.trace("对象: {} 不存在字段: {}", clazz.getSimpleName(), name);
+                    }
+
+                }
+
+                if(one) {
+                    break;
+                }
+            }
+            return resObj;
+        } catch (SQLException e) {
+            log.error("执行查询出错: ", e);
+        } catch (InstantiationException e) {
+            throw new RuntimeException(e);
+        } catch (IllegalAccessException e) {
+            throw new RuntimeException(e);
+        } catch (NoSuchMethodException e) {
+            throw new RuntimeException(e);
+        } catch (InvocationTargetException e) {
+            throw new RuntimeException(e);
+        } finally {
+            JdbcUtils.closeResultSet(rs);
+        }
+        return null;
+    }
+
+    public void setParamsToStatement(PreparedStatement ps) throws SQLException {
+        for(int i = 1; i == params.size(); i++) {
+            Object p = params.get(i - 1);
+            if(p == null) {
+                continue;
+            }
+            Class<?> type = p.getClass();
+            if (type == String.class)
+                ps.setString(i, (String) p);
+            else if (type == BigDecimal.class)
+                ps.setBigDecimal(i, (BigDecimal) p);
+            else if (type == boolean.class || type == Boolean.class)
+                ps.setBoolean(i, (Boolean) p);
+            else if (type == long.class || type == Long.class)
+                ps.setLong(i, (Long) p);
+            else if (type == int.class || type == Integer.class)
+                ps.setInt(i, (Integer) p);
+            else if (type == char.class)
+                ps.setString(i, String.valueOf((char) p));
+            else if (type == byte.class || type == Byte.class)
+                ps.setByte(i, (Byte) p);
+            else if (type == byte[].class || type == Byte[].class)
+                ps.setBytes(i, (byte[]) p);
+            else if (type == short.class || type == Short.class)
+                ps.setShort(i, (Short) p);
+            else if (type == float.class || type == Float.class)
+                ps.setFloat(i, (Float) p);
+            else if (type == double.class || type == Double.class)
+                ps.setDouble(i, (Double) p);
+            else if (type == Date.class) {
+                java.sql.Date sqlDate = new java.sql.Date(((Date) p).getTime());
+                ps.setDate(i, sqlDate);
+            } else if (type == Date.class) {
+                java.sql.Time sqlDate = new java.sql.Time(((Date) p).getTime());
+                ps.setTime(i, sqlDate);
+            }  else if (type == Date.class) {
+                java.sql.Timestamp sqlDate = new java.sql.Timestamp(((Date) p).getTime());
+                ps.setTimestamp(i, sqlDate);
+            } else {
+                throw new SQLException(type + " :未知的类型");
+            }
+
+        }
+    }
+
+
+
+
+}
---
+++ lotus-orm/src/main/java/lotus/or/orm/pool/DataSourceConfig.java
@@ -0,0 +1,101 @@
+package lotus.or.orm.pool;
+
+public class DataSourceConfig {
+    public String url;
+    public String username;
+    public String password;
+    public String driverName;
+    public int maxConnection = 100;
+    public int minConnection = 10;
+    public int heartbeatFreqSecs = 30;//心跳检测间隔
+    public int heartbeatTimeoutSeconds = 5;//心跳检测超时时间
+    public int loginTimeout = 30;//获取连接超时时间 秒
+
+    public DataSourceConfig() {
+    }
+
+    public DataSourceConfig(String url, String username, String password) {
+        this.url = url;
+        this.username = username;
+        this.password = password;
+    }
+
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getDriverName() {
+        if(driverName == null) {
+            if(url.startsWith("jdbc:mysql")) {
+                return "com.mysql.jdbc.Driver";
+            }
+        }
+
+        return driverName;
+    }
+
+    public void setDriverName(String driverName) {
+        this.driverName = driverName;
+    }
+
+    public int getMaxConnection() {
+        return maxConnection;
+    }
+
+    public void setMaxConnection(int maxConnection) {
+        this.maxConnection = maxConnection;
+    }
+
+    public int getMinConnection() {
+        return minConnection;
+    }
+
+    public void setMinConnection(int minConnection) {
+        this.minConnection = minConnection;
+    }
+
+    public int getHeartbeatFreqSecs() {
+        return heartbeatFreqSecs;
+    }
+
+    public void setHeartbeatFreqSecs(int heartbeatFreqSecs) {
+        this.heartbeatFreqSecs = heartbeatFreqSecs;
+    }
+
+    public int getHeartbeatTimeoutSeconds() {
+        return heartbeatTimeoutSeconds;
+    }
+
+    public void setHeartbeatTimeoutSeconds(int heartbeatTimeoutSeconds) {
+        this.heartbeatTimeoutSeconds = heartbeatTimeoutSeconds;
+    }
+
+    public int getLoginTimeout() {
+        return loginTimeout;
+    }
+
+    public void setLoginTimeout(int loginTimeout) {
+        this.loginTimeout = loginTimeout;
+    }
+}
