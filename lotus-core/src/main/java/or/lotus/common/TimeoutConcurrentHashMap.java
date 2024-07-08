ceabe27bbb51ae0f677537d6e2d92b13c1c54223
yangfan <982465979@qq.com>
2024-07-08 09:50:48 +0800

    增加队列(插入时有排序), 抛弃整体循环判断超时

--- lotus-core/src/main/java/or/lotus/common/TimeoutConcurrentHashMap.java
+++ lotus-core/src/main/java/or/lotus/common/TimeoutConcurrentHashMap.java
@@ -7,6 +7,9 @@ import java.util.Map;
 import java.util.Timer;
 import java.util.TimerTask;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.DelayQueue;
+import java.util.concurrent.Delayed;
+import java.util.concurrent.TimeUnit;
 
 /**
  * 简易超时hashmap
@@ -15,17 +18,19 @@ import java.util.concurrent.ConcurrentHashMap;
  */
 public class TimeoutConcurrentHashMap<K, V> extends TimerTask implements Closeable {
     private ConcurrentHashMap<K, ExpireWrap> map;
+    private DelayQueue<ExpireWrap<V>> delayQueue;
     private Timer timer;
 
     private int checkDiff;
 
     public TimeoutConcurrentHashMap() {
-        this(0, 1000);
+        this(16, 1000);
     }
 
     public TimeoutConcurrentHashMap(int initialCapacity, int checkDiff) {
         this.checkDiff = checkDiff;
         map = new ConcurrentHashMap<>(initialCapacity);
+        delayQueue = new DelayQueue<>();
         timer = new Timer();
         timer.schedule(this, checkDiff, checkDiff);
     }
@@ -37,7 +42,7 @@ public class TimeoutConcurrentHashMap<K, V> extends TimerTask implements Closeab
     /***
      * 此方法加入的对象将永远不会超时
      * @param k
-     * @param v
+     * @param v 此对象需要自己实现 equals 否则判断超时, 移除对象会出现问题
      */
     public void put(K k, V v) {
         put(k, v, -1);
@@ -46,14 +51,16 @@ public class TimeoutConcurrentHashMap<K, V> extends TimerTask implements Closeab
     /***
      *
      * @param k
-     * @param v
+     * @param v 此对象需要自己实现 equals 否则判断超时, 移除对象会出现问题
      * @param sec 超时时间
      */
     public void put(K k, V v, int sec) {
        if(sec > 0) {
-           map.put(k, new ExpireWrap(v, System.currentTimeMillis() + sec * 1000));
+           ExpireWrap obj = new ExpireWrap(k, v, System.currentTimeMillis() + sec * 1000);
+           map.put(k, obj);
+           delayQueue.add(obj);
        } else {
-           map.put(k, new ExpireWrap(v, -1));
+           map.put(k, new ExpireWrap(k, v, -1));
        }
     }
 
@@ -69,6 +76,7 @@ public class TimeoutConcurrentHashMap<K, V> extends TimerTask implements Closeab
     public V remove(K k) {
         ExpireWrap val = map.remove(k);
         if(val != null) {
+            delayQueue.remove(val);
             return (V) val.obj;
         }
         return null;
@@ -94,11 +102,22 @@ public class TimeoutConcurrentHashMap<K, V> extends TimerTask implements Closeab
 
     @Override
     public void run() {
-        for(Map.Entry<K, ExpireWrap> obj : map.entrySet()) {
+        ExpireWrap v = null;
+        do {
+            try {
+                v = delayQueue.poll(1, TimeUnit.SECONDS);
+                if(v != null) {
+                    map.remove(v.k);
+                }
+            } catch (InterruptedException e) {
+                v = null;
+            }
+        } while(v != null);
+        /*for(Map.Entry<K, ExpireWrap> obj : map.entrySet()) {
             if(obj.getValue().isTimeout()) {
                 map.remove(obj.getKey());
             }
-        }
+        }*/
     }
 
     @Override
@@ -106,17 +125,38 @@ public class TimeoutConcurrentHashMap<K, V> extends TimerTask implements Closeab
         shutdown();
     }
 
-    private class ExpireWrap <V> {
+    private class ExpireWrap <V> implements Delayed {
+        K k;
         V obj;
         long expire;
 
-        public ExpireWrap(V obj, long expire) {
+        public ExpireWrap(K k, V obj, long expire) {
+            this.k = k;
             this.obj = obj;
             this.expire = expire;
         }
 
         public boolean isTimeout() {
             return expire != -1 && System.currentTimeMillis() > expire;
+        }
+
+        @Override
+        public long getDelay(TimeUnit unit) {
+            //延迟任务是否到时就是按照这个方法判断如果返回的是负数则说明到期
+            // 否则还没到期
+            return unit.convert(expire - System.currentTimeMillis(), TimeUnit.MILLISECONDS);
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            ExpireWrap other = (ExpireWrap) o;
+            return this.obj.equals(other.obj);
+        }
+
+        @Override
+        public int compareTo(Delayed o) {
+            ExpireWrap other = (ExpireWrap) o;
+            return (int)((expire - System.currentTimeMillis()) - (other.expire - System.currentTimeMillis()));
         }
     }
 }
