a0d97340f772abc66219960ee1b32dd18582471a
yangfan <982465979@qq.com>
2025-01-16 16:10:08 +0800

    文件类型的表单支持多个名称相同的值

--- lotus-core/src/main/java/or/lotus/core/http/restful/RestfulFormData.java
+++ lotus-core/src/main/java/or/lotus/core/http/restful/RestfulFormData.java
@@ -3,10 +3,22 @@ package or.lotus.core.http.restful;
 
 import java.io.File;
 import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.List;
 
 public abstract class RestfulFormData {
 
     public abstract RestfulFormDataItem getFormDataItem(String name);
+    public abstract List<RestfulFormDataItem> getFormDataItems(String name);
+
+    public List<String> getFormDataItemValues(String name) {
+        List<RestfulFormDataItem> items = getFormDataItems(name);
+        List<String> values = new ArrayList(items.size());
+        for(RestfulFormDataItem item : items) {
+            values.add(item.getValue());
+        }
+        return values;
+    }
 
     public String getFormDataItemValue(String name) {
         RestfulFormDataItem item = getFormDataItem(name);
--- lotus-netty-http/src/main/java/or/lotus/http/netty/NettyFormData.java
+++ lotus-netty-http/src/main/java/or/lotus/http/netty/NettyFormData.java
@@ -7,6 +7,10 @@ import io.netty.handler.codec.http.multipart.InterfaceHttpData;
 import or.lotus.core.http.restful.RestfulFormData;
 import or.lotus.core.http.restful.RestfulFormDataItem;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class NettyFormData extends RestfulFormData {
     //解析文件大小(如果是:minSize则会过滤掉16K以下的文件,这个则不限制文件最小长度)
     private static final HttpDataFactory factory = new DefaultHttpDataFactory(DefaultHttpDataFactory.MAXSIZE);
@@ -30,5 +34,21 @@ public class NettyFormData extends RestfulFormData {
         }
 
         return null;
+    }
+
+    @Override
+    public List<RestfulFormDataItem> getFormDataItems(String name) {
+        if(request.isMultipart()) {
+            List<InterfaceHttpData> items = decoder.getBodyHttpDatas(name);
+            if(items != null && items.size() > 0) {
+                List<RestfulFormDataItem> list = new ArrayList(items.size());
+                for(InterfaceHttpData item : items) {
+                    list.add(new NettyFormDataItem(item));
+                }
+                return list;
+            }
+        }
+
+        return new ArrayList();
     }
 }
